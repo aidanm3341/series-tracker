8fd5efc520aeff5d812ca60afd2420e110519234
Aidan McPhelim <aidanmcp@hotmail.co.uk>
2021-03-25 18:12:37 +0000

    added create table statement

--- src/dal/SQLiteSeriesDAL.cpp
+++ src/dal/SQLiteSeriesDAL.cpp
@@ -1,22 +1,29 @@
 #include <SQLiteSeriesDAL.h>
 
 SQLiteSeriesDAL::SQLiteSeriesDAL() : db(sqlite::database("series.db"))
-{}
+{
+    db <<
+         "CREATE TABLE IF NOT EXISTS Series ("
+         "   _id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"
+         "   maxSeries INT,"
+         "   currentSeries INT"
+         ");";
+}
 
 std::vector<Series> SQLiteSeriesDAL::loadSeries()
 {
     try
     {
-        db << "SELECT name,  FROM Series;"
+        db << "SELECT name, maxSeries, currentSeries FROM Series;"
         >> [&](std::string name, int maxSeries, int currentSeries) {
-            // breakpoint
+            std::cout << name << std::endl;
         };
     }
     catch(std::exception& e)
     {
         std::cout << e.what() << std::endl;
     }
-    
+
     return std::vector<Series>();
 }
 
